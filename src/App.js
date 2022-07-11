212a5dbdeef1fb6f79a4b0c8b041708c2468e3ff
Vladislav <porvinvlad.2019@gmail.com>
2022-07-11 17:49:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import { Controls } from './components/Controls';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { ALL_COUNTRIES } from './config';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  console.log(countries);
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main>
+        <Controls />
+      </Main>
+    </>
   );
 }
 
